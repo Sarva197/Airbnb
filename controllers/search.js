bfc7f51728f0b08e472518880816395ab3a866c8
Sarva197 <sarvagnalv179@gmail.com>
2025-03-11 18:08:37 +0530

    project Done

--- controllers/search.js
+++ controllers/search.js
@@ -2,7 +2,6 @@ const Listing = require("../models/listing");
 
 module.exports.search = async (req, res) => {
     let { name } = req.query;
-    console.log(name);
     const allListings = await Listing.find({ title: new RegExp(name, 'i') });
     res.render("listings/search", { allListings });
 };
